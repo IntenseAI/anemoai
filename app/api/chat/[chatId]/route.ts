32d33211c5a3eb92911fb7b9b36e65c27376a383
AKSAATHWIK <151855972+IntenseAI@users.noreply.github.com>
2023-11-25 16:59:17 +0530

    Update route.ts

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -46,7 +46,7 @@ export async function POST(
     });
 
     if (!companion) {
-      return new NextResponse("AI Companion not found", { status: 404 });
+      return new NextResponse("Companion not found", { status: 404 });
     }
 
     const name = companion.id;
@@ -55,7 +55,7 @@ export async function POST(
     const companionKey = {
       companionName: name!,
       userId: user.id,
-      modelName: "meta/llama-2-70b-chat",
+      modelName: "llama2-13b",
     };
     const memoryManager = await MemoryManager.getInstance();
 
@@ -85,7 +85,7 @@ export async function POST(
     // Call Replicate for inference
     const model = new Replicate({
       model:
-      "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
+        "a16z-infra/llama-2-13b-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
       input: {
         max_length: 2048,
       },
