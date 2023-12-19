ecd9ffeb58dab796dfe158db063b5a5d5e1d5c5d
AKSAATHWIK <151855972+IntenseAI@users.noreply.github.com>
2023-12-19 09:33:29 +0530

    Final

---
+++ components/home/getStarted.tsx
@@ -0,0 +1,28 @@
+"use client";
+import Link from "next/link";
+import ScrollLink from "./scrollLink";
+import { useUser } from '@clerk/nextjs';
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+
+
+export default function getStarted() {
+  const user = useUser();
+  const { isSignedIn, isLoaded } = user;
+  
+  return (
+    <div className="hidden lg:flex gap-8">
+        {
+            isLoaded && <>
+                {isSignedIn ? <>
+                    <Link href="/dashboard" className="btn_gradient px-14 py-3">Get Started</Link>
+          </>:<>
+                    <SignInButton afterSignInUrl='/dashboard' afterSignUpUrl='/dashboard'>
+                        Get Started
+                    </SignInButton>
+                       
+                </>}
+            </>
+        }
+    </div>
+  );
+}
